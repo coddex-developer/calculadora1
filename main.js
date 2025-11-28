84a4641c4e35fcfa73e05c5779a43788f540391e
CoddeX <76017481+coddex-developer@users.noreply.github.com>
2025-11-28 15:31:04 -0300

    update main.js

--- main.js
+++ main.js
@@ -95,6 +95,7 @@ sum.addEventListener("click", ev => {
     multiplicacao.textContent = A * B;
     divisao.textContent = A / B;
     options.classList.add("func");
+    return CopyList(A, B);
   } else {
     error.classList.add("alert");
     closeError.addEventListener("click", () => {
