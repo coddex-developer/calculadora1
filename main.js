58dedaa3c0e1bd1e2bb323e611f06ca595c48536
CoddeX <76017481+coddex-developer@users.noreply.github.com>
2025-12-03 16:53:24 -0300

    update main.js

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ const divisao = document.getElementById("divisao");
 document.querySelector("form").addEventListener("input", ev => ev.preventDefault())
 
 const runOperations = (numberA, numberB) => {
-  console.log(typeof(+numberA))
+  +numberA
+  console.log(typeof(numberA))
   soma.textContent = numberA + numberB
   subtracao.textContent = numberA - numberB
   divisao.textContent = numberA / numberB
