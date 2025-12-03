8254f4727073f11e0a3721a9f1ef4daa559780de
CoddeX <76017481+coddex-developer@users.noreply.github.com>
2025-12-03 16:07:45 -0300

    update main.js

--- main.js
+++ main.js
@@ -11,22 +11,19 @@ const subtracao = document.getElementById("subtracao");
 const multiplicacao = document.getElementById("multiplicacao");
 const divisao = document.getElementById("divisao");
 
-const objCalc = {
-  sum: inputA + inputB,
-  subtract: inputA - inputB,
-  division: inputA / inputB
-};
-const {sum} = objCalc;
-const {subtract} = objCalc;
-const {division} = objCalc;
-
 document.querySelector("form").addEventListener("input", ev => ev.preventDefault())
+
+const runOperations = (numberA, numberB) => {
+  const valuesOperation = [{
+    soma: numberA + numberB,
+    subtract: numberA - numberB,
+    division: numberA / numberB,
+    multiple: numberA * numberB
+  }]
+}
 
 showSum.addEventListener("click", (ev) => {
   ev.preventDefault();
-  if(inputA == "") return alert("Preencha todos os campus!");
-  if(inputB == "") return alert("Preencha todos os campus!");
-  if(tyoeof(inputA) !== number) return alert("Número inválido");
-  if(typeof(inputB) !== number) return alert("Número inválido!");
-  
+
+
 })
